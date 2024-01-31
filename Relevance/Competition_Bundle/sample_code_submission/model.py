5001c5b67e635ce2bec14321af865e13fd363447
paulohenriquecrs <48411933+paulohenriquecrs@users.noreply.github.com>
2024-01-31 13:02:30 +0100

    Add files via upload

--- Relevance/Competition_Bundle/sample_code_submission/model.py
+++ Relevance/Competition_Bundle/sample_code_submission/model.py
@@ -1,15 +1,20 @@
-""" The `Model` file encapsulates a model class structured in the style of scikit-learn. 
-    This model class is equipped with three essential methods:
-    - __init__: Initializes the model.
-    - fit: Trains the model.
-    - predict: Utilizes the model to make predictions.
-Created by: Ihsan Ullah
-Created on: 10 Jan, 2024
-"""
+# Model file which contains a model class in scikit-learn style
+# Model class must have these 3 methods
+# - __init__: initializes the model
+# - fit: trains the model
+# - predict: uses the model to perform predictions
+#
+# Created by: Ihsan Ullah
+# Created on: 10 Jan, 2024
+
 # ----------------------------------------
 # Imports
 # ----------------------------------------
-from sklearn.ensemble import GradientBoostingClassifier
+from transformers import BertForSequenceClassification
+import torch
+from tqdm import tqdm
+
+from tqdm.notebook import tqdm
 # from sklearn.naive_bayes import MultinomialNB
 
 
@@ -31,19 +36,30 @@ class Model:
         None
         """
         print("[*] - Initializing Classifier")
-        self.clf = GradientBoostingClassifier()
+        self.id2label = {0: "least_relevant", 1: "second_least_relevant", 2: "second_most_relevant", 3: "most_relevant"}
+        self.label2id = {"least_relevant": 0, "second_least_relevant": 1, "second_most_relevant": 2, "most_relevant": 3}
+        
+        self.model = BertForSequenceClassification.from_pretrained(
+            "bert-base-uncased",
+            problem_type="multi_label_classification",
+            num_labels=4,
+            id2label=self.id2label,
+            label2id=self.label2id
+            )
 
-    def fit(self, X, y):
+        # set device (cpu for local machines)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(self.device)
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=5e-5)
+
+    def fit(self, dataloader, num_epochs=1):
         """
         This function trains the model provided training data
 
         Parameters
         ----------
-        X: 2D numpy array
-            training data matrix of dimension num_train_examples * num_features
-            each column is a feature and each row a datapoint
-        y: 1D numpy array
-            training label matrix of dimension num_train_samples
+        train_dataloader:   Pytorch Dataloader with training data
+        num_epochs:         Integer representing the number of epochs fro training
 
         Returns
         -------
@@ -51,26 +67,53 @@ class Model:
         """
 
         print("[*] - Training Classifier on the train set")
-        self.clf.fit(X, y)
+        for epoch in range(num_epochs):
+            self.model.train()
+            total_loss = 0
+    
+            for batch in tqdm(dataloader, desc=f"Epoch {epoch + 1}"):
+                inputs, labels = batch
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+    
+                self.optimizer.zero_grad()
+                outputs = self.model(inputs, labels=labels)
+                loss = outputs.loss
+                total_loss += loss.item()
+    
+                loss.backward()
+                self.optimizer.step()
+    
+            avg_loss = total_loss / len(dataloader)
+            print(f"Average training loss: {avg_loss}")
 
-    def predict(self, X):
+
+    def predict(self, dataloader):
 
         """
         This function predicts labels on test data.
 
         Parameters
         ----------
-        X: 2D numpy array
-            test data matrix of dimension num_test_examples * num_features
-            each column is a feature and each row a datapoint
+        X: Pytorch Dataloader with test data
 
         Returns
         -------
-        y: 1D numpy array
-            predicted labels
+        y_pred: List of predicted labels
+        y_true: List of true test labels
         """
 
         print("[*] - Predicting test set using trained Classifier")
-        y = self.clf.predict(X)
+        self.model.eval()
+        predictions = []
+        true_labels = []
+        
+        for batch in dataloader:
+            inputs, labels = batch
+            with torch.no_grad():
+                outputs = self.model(inputs)
+        
+            logits = outputs.logits
+            predictions.extend(torch.argmax(logits, dim=1).tolist())
+            true_labels.extend(labels.tolist())
 
-        return y
+        return predictions, true_labels
